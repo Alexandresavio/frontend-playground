41b616920bafe9ab1992a2757736ee0a20454e32
savio <alexandresaviosouza@gmail.com>
2025-08-12 16:21:01 -0300

    adicionando arquivos

---
+++ javascript/coder-JS/1_fundamentos/4_tipagem.js
@@ -0,0 +1,19 @@
+/*
+ * O JavaScript é uma linguagem fracamente tipada, isso significa que  “ele” tentará converter em tempo de execução os dados no valor.
+ */
+
+// aqui a variavel 'qulaquer' recebe um valor do tipo string
+let qualquer = "legal";
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// aqui a variavel 'qulaquer' recebe um valor do tipo numerico(pode ser inteiro ou ponto flutuante)
+qualquer =3.1514;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+//Evitar nomes genéricos e siglas
+let valor = "";
+let numero = 1;
+let pqp = false // produto quimico perigoso
+
